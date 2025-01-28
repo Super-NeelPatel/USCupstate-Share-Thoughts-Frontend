a8bea46691f631b6e4ce8a4990a1a3eff684956f
Neel Patel <53189954+Super-NeelPatel@users.noreply.github.com>
2025-01-27 22:33:13 -0500

    Update SignupPage.jsx

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -24,7 +24,7 @@ const Signup = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:8000/api/users/signup",
+        "https://blog-site-frontend-dlxl.onrender.com/api/users/signup",
         formData,
         {
           headers: {
